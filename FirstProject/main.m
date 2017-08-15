3c14d1ecc03a6bc245428cb96fda2000b5cad83b
prabodh44 <prabodtuladhar44@gmail.com>
2017-08-15 15:55:05 +0545

    Practised the loops, break and continue statements

--- FirstProject/main.m
+++ FirstProject/main.m
@@ -11,7 +11,9 @@
 //where the variable is not defined
 extern int externFirstVar;
 extern int externSecondVar;
-
+void exampleOfWhileLoop();
+void exampleOfForLoop();
+void exampleOfDoLoop();
 //use of the define pre-processor
 #define PI 3.1416
 
@@ -19,7 +21,6 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
-        //int outSideVar = 8;
         
         int firstNum;
         int secondNum;
@@ -49,10 +50,52 @@ int main(int argc, const char * argv[]) {
         const double G = 6.67E-5;
         NSLog(@"Universal Gravitational Constant = %f", G);
         
+        NSLog(@"Example of while...loop");
+        exampleOfWhileLoop();
         
+        NSLog(@"Example of for..loop");
+        exampleOfForLoop();
+        
+        NSLog(@"Example of do...loop");
+        exampleOfDoLoop();
         
     }
     return 0;
 }
+
+void exampleOfWhileLoop(){
+    int cnt = 0;
+    while(cnt <= 10){
+        if(cnt % 2 == 0){
+            NSLog(@"The even number are %d", cnt);
+        }
+        cnt++;
+    }
+}
+
+void exampleOfForLoop(){
+    for(int i = 0; i < 10; i++){
+        NSLog(@"%d\t", i);
+        if(i >= 5){
+            break;
+        }
+    }
+    
+    NSLog(@"Broken!!");
+}
+
+void exampleOfDoLoop(){
+    int cnt = 0;
+    do{
+        cnt = cnt + 1;
+        if(cnt == 6){
+            continue;
+        }
+        NSLog(@"%d\t", cnt);
+    }while(cnt < 10);
+    
+}
+
+
 
 
