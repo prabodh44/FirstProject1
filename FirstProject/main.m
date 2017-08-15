cc3da2eee6ce18d3fde954cc5b36e9f3914fc8c0
aakash tandukar <aakash_tandukar@hotmail.com>
2017-08-15 13:06:21 +0545

    Added the code for simple arithnetic operations

--- FirstProject/main.m
+++ FirstProject/main.m
@@ -8,11 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        int firstNum;
+        int secondNum;
+        NSLog(@"Hello World\n");
+        NSLog(@"Simple mathmatical calculations\n");
+        firstNum = 10;
+        secondNum = 3;
         
-        NSLog(@"i am new here");
+        int sum = firstNum + secondNum;
+        int diff = firstNum - secondNum;
+        int product = firstNum * secondNum;
+        float div = firstNum / secondNum;
+        
+        NSLog(@"The sum is %d\n", sum);
+        NSLog(@"The difference  is %d\n", diff);
+        NSLog(@"The product is %d\n", product);
+        NSLog(@"The quotient is %f\n", div);
+        
+        
+        
     }
     return 0;
 }
