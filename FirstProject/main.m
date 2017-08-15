78bd2b22da61ba6f3078b8e114b22e73d9bd1a7c
prabodh44 <prabodtuladhar44@gmail.com>
2017-08-15 16:55:30 +0545

    Deleted code for all functions

--- FirstProject/main.m
+++ FirstProject/main.m
@@ -11,9 +11,7 @@
 //where the variable is not defined
 extern int externFirstVar;
 extern int externSecondVar;
-void exampleOfWhileLoop();
-void exampleOfForLoop();
-void exampleOfDoLoop();
+
 //use of the define pre-processor
 #define PI 3.1416
 
@@ -50,51 +48,11 @@ int main(int argc, const char * argv[]) {
         const double G = 6.67E-5;
         NSLog(@"Universal Gravitational Constant = %f", G);
         
-        NSLog(@"Example of while...loop");
-        exampleOfWhileLoop();
-        
-        NSLog(@"Example of for..loop");
-        exampleOfForLoop();
-        
-        NSLog(@"Example of do...loop");
-        exampleOfDoLoop();
         
     }
     return 0;
 }
 
-void exampleOfWhileLoop(){
-    int cnt = 0;
-    while(cnt <= 10){
-        if(cnt % 2 == 0){
-            NSLog(@"The even number are %d", cnt);
-        }
-        cnt++;
-    }
-}
-
-void exampleOfForLoop(){
-    for(int i = 0; i < 10; i++){
-        NSLog(@"%d\t", i);
-        if(i >= 5){
-            break;
-        }
-    }
-    
-    NSLog(@"Broken!!");
-}
-
-void exampleOfDoLoop(){
-    int cnt = 0;
-    do{
-        cnt = cnt + 1;
-        if(cnt == 6){
-            continue;
-        }
-        NSLog(@"%d\t", cnt);
-    }while(cnt < 10);
-    
-}
 
 
 
