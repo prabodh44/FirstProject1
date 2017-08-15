648e7b8aea32a4886de5e9cb1e3ec8491cd590d5
aakash tandukar <aakash_tandukar@hotmail.com>
2017-08-15 11:25:24 +0545

    Initial Commit

---
+++ FirstProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstProject
+//
+//  Created by ith on 8/15/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
