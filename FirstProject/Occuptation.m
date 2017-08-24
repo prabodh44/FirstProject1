f5bc9b189a6453b96ec338696984d07f3be910d5
prabodh44 <prabodtuladhar44@gmail.com>
2017-08-24 15:05:54 +0545

    Added the Occupation class to practice keypath

---
+++ FirstProject/Occuptation.m
@@ -0,0 +1,13 @@
+//
+//  Occuptation.m
+//  FirstProject
+//
+//  Created by ith on 8/24/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import "Occuptation.h"
+
+@implementation Occuptation
+
+@end
