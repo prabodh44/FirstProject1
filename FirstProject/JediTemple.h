d4c1782f78ea09154d32c88da31b7e3ee1e7d6a8
prabodh44 <prabodtuladhar44@gmail.com>
2017-08-25 13:05:07 +0545

    Added the Jedi Temple class to [practice sending blocks into methods

---
+++ FirstProject/JediTemple.h
@@ -0,0 +1,13 @@
+//
+//  JediTemple.h
+//  FirstProject
+//
+//  Created by ith on 8/25/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JediTemple : NSObject
+
+@end
---
+++ FirstProject/JediTemple.m
@@ -0,0 +1,13 @@
+//
+//  JediTemple.m
+//  FirstProject
+//
+//  Created by ith on 8/25/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import "JediTemple.h"
+
+@implementation JediTemple
+
+@end
