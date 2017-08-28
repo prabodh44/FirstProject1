428b3a5cf4fc0ff752f36bf093f016e0c7641c24
prabodh44 <prabodtuladhar44@gmail.com>
2017-08-28 15:10:08 +0545

    Created the Girl and Sweater class to practice memory management

---
+++ FirstProject/Girl.h
@@ -0,0 +1,13 @@
+//
+//  Girl.h
+//  FirstProject
+//
+//  Created by ith on 8/28/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Girl : NSObject
+
+@end
---
+++ FirstProject/Girl.m
@@ -0,0 +1,13 @@
+//
+//  Girl.m
+//  FirstProject
+//
+//  Created by ith on 8/28/17.
+//  Copyright © 2017 ith. All rights reserved.
+//
+
+#import "Girl.h"
+
+@implementation Girl
+
+@end
